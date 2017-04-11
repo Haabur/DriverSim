f786d19f32e6db5d36a592af9cef257063fd7047
taitjc1 <taitjc1@LABPC03-C320.soc.local>
2017-04-12 11:34:57 +1200

    First Build

--- DriverSimulator/src/driversimulator/DriverSimulator.java
+++ DriverSimulator/src/driversimulator/DriverSimulator.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package driversim;
+package driversimulator;
 
 /**
  *
  * @author taitjc1
  */
-public class DriverSim {
+public class DriverSimulator {
 
     /**
      * @param args the command line arguments
