04bf9ca37eec1aaa501fd2edfe112bfca8a8d187
taitjc1 <taitjc1@LABPC03-C320.soc.local>
2017-05-30 12:19:11 +1200

    Changed the seed input

--- DriverSimulator/src/driversimulator/DriverSimulator.java
+++ DriverSimulator/src/driversimulator/DriverSimulator.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package driversimulator;
+
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -17,12 +19,28 @@ public class DriverSimulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Random generator = new Random(6336);
+        String n = "";
+        int seed = 0;
+        boolean goodSeed = true;
+        Scanner reader = new Scanner(System.in);  // Reading from System.in       
+        System.out.println("Please enter a seed.");
+        while (goodSeed) {
+            n = reader.next();
+            if (n.length() < 10 && tryParseInt(n)) {
+                seed = Integer.parseInt(n);
+                goodSeed = false;
+            }
+            else{
+                System.out.println("Please enter a seed that is a integer with less than 10 digits.");
+            }
+        }
+
+        Random generator = new Random(seed);
         //double random = generator.nextDouble();
         System.out.println(generator.nextDouble());
-       
+
         Location[] locations = new Location[4];
-        
+
         locations[0] = new Location("Mayfair", 3, 1);
         locations[1] = new Location("Akina", 0, 2);
         locations[2] = new Location("Stortford Lodge", 1, 3);
@@ -30,16 +48,25 @@ public class DriverSimulator {
 
         for (int i = 1; i < 6; i++) {
             String name = "Driver " + i;
-            
-            Location startingPlace = locations[(int)(generator.nextDouble()*4)];
+
+            Location startingPlace = locations[(int) (generator.nextDouble() * 4)];
             System.out.println(name + " has started in " + startingPlace.getName());
             Driver zoom = new Driver(name, startingPlace);
             while (zoom.inHastings() == true) {
                 zoom.chooseDirection(generator.nextDouble(), generator.nextDouble(), locations);
                 System.out.println(name + " is in " + zoom.locationName());
-                
 
             }
+        }
+
+    }
+
+    static boolean tryParseInt(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 
@@ -51,7 +78,6 @@ class Driver {
     String name = "";
     Location location = null;
     boolean inHastings = true;
-    
 
     public Driver(String _name, Location startLoc) {
         this.location = startLoc;
@@ -68,13 +94,13 @@ class Driver {
             this.location = locations[this.location.Right()];
             this.locationName = this.location.getName();
         }
-        if (random2 > 0.9){
+        if (random2 > 0.9) {
             this.inHastings = false;
             this.locationName = "Outside City";
         }
     }
-    
-    public String locationName(){
+
+    public String locationName() {
         return this.locationName;
     }
 
